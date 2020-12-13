958463ddd96823d086c6b5b049a1bbc543cc8dfb
tbsnfruits <llinoa22@gmail.com>
2020-12-13 23:41:03 +0900

    day 03

--- src/advent2020/core.clj
+++ src/advent2020/core.clj
@@ -12,8 +12,7 @@
   "finds numbers in numbs that sums into sum"
   (clojure.set/intersection
     (set numbs)
-    (set (map #(- sum %) numbs)))
-  )
+    (set (map #(- sum %) numbs))))
 
 ;Day 1-2 :A+B = 2020 - C
 (let [numbs (list-numb "resources/day01.txt")
@@ -34,11 +33,11 @@
 
 (defn count-valid-pass [predicate path]
   (let [passlist (parse-passlist path)]
-   (->> (map #(apply predicate %) passlist)
-        (filter true?)
-        count)))
+    (->> (map #(apply predicate %) passlist)
+         (filter true?)
+         count)))
 
-(count-valid-pass password-old? "resources/day02.txt")
+;(count-valid-pass password-old? "resources/day02.txt")
 
 ;;Day 2-2
 (defn password-new? [pos1 pos2 character password]
@@ -50,4 +49,35 @@
       true)
     ))
 
-(count-valid-pass password-new? "resources/day02.txt")
+;(count-valid-pass password-new? "resources/day02.txt")
+
+;;Day 3
+(def pattern (vec (listify-txt "resources/day03.txt")))
+
+(defn listify-coordinate [pattern right down]
+  (let [height (count pattern)
+        width (count (first pattern))]
+    (for [i (range 400)
+          :let [x (inc (* right i))
+                y (inc (* down i))]
+          :when (<= y height)]
+      [x y])))
+
+(defn get-point [pattern [x y]]
+  (let [width (count (first pattern))
+        x' (mod x width)]
+    (-> (get pattern (dec y))
+        (get (dec (if (= 0 x') width x'))))))
+
+(defn map-path [pattern right down]
+  (map #(get-point pattern %) (listify-coordinate pattern right down)))
+
+(defn count-tree-on-path [pattern right down]
+  (->> (map-path pattern right down)
+       (filter #(= % \#))
+       count))
+
+(comment
+  (count-tree-on-path pattern 3 1)
+  (->> (map #(apply count-tree-on-path pattern %) [[1 1] [3 1] [5 1] [7 1] [1 2]])
+       (apply *)))
