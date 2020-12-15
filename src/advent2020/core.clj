67c1819fa9c3fc9ec5244ee5c44db1d4748cecff
tbsnfruits <llinoa22@gmail.com>
2020-12-16 01:56:16 +0900

    Day 04

--- src/advent2020/core.clj
+++ src/advent2020/core.clj
@@ -84,21 +84,41 @@
 
 
 ;;Day 4
-(defn vec-data [str]
-  (update (string/split str #":") 0 keyword))
+(defn split [regex s]
+  (string/split s regex))
 
-(def day04-data
-  (map #(string/split % #" |\n") (string/split (slurp "resources/day04.txt") #"\n\n")))
+(defn parse-passport [serialized]
+  (->> serialized                                           ;"a:1 b:2 c:3"
+       (split #" |\n")                                      ;["a:1" "b:2 "c:3"]
+       (map #(split #":" %))                                ;[["a" "1"] ["b" "2"] ["c" "3"]]
+       (into {})                                            ;{"a" "1", "b" "2", "c" "3"}
+       clojure.walk/keywordize-keys))                       ;{:a "1", :b "2", :c "3"}
 
-(defn map-data [data]
-  (reduce (fn [acc coll]
-           (conj acc (apply merge {} (map vec-data coll))))
-         [] data))
 
-(defn valid-document? [passport]
-  "해당 맵이 조건에 맞는지 확인"
-  (->> (map #(get passport %) [:byr :iyr :eyr :hgt :hcl :ecl :pid])
-       (every? (complement nil?))))
+(defn valid-document? [{:keys [byr iyr eyr hgt hcl ecl pid]}]
+  (and byr iyr eyr hgt hcl ecl pid
+       (<= 1920 (Integer/parseInt byr) 2002)                ;byr
+       (<= 2010 (Integer/parseInt iyr) 2020)                ;iyr
+       (<= 2020 (Integer/parseInt eyr) 2030)                ;eyr
 
-(defn count-valid-document [data]
-  (count (filter true? (map valid-document? (map-data data)))))
+       (let [[_ numb unit]                                  ;hgt
+             (re-matches #"(\d+)(cm|in)" hgt)]
+         (case unit
+           "cm" (<= 150 (Integer/parseInt numb) 193)
+           "in" (<= 59 (Integer/parseInt numb) 76)
+           false))
+
+       (re-matches #"#[0-9a-f]{6}" hcl)                     ;hcl
+       (contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl) ;ecl
+       (re-matches #"[0-9]{9}" pid)                         ;pid
+       )
+  )
+
+(comment
+  (->> (slurp "resources/day04.txt")
+       (split #"\n\n")
+       (map parse-passport)
+       (filter valid-document?)
+       count))
+
+
